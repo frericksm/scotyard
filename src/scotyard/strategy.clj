807e5d665e8f94d05228d98da854265200aebbf6
Michael Frericks <Michael-Frericks@web.de>
2015-04-11 00:17:57 +0200

    fix fn children

--- src/scotyard/strategy.clj
+++ src/scotyard/strategy.clj
@@ -5,7 +5,7 @@
 
 (defn create-alphabeta 
   ""
-  [branch? children heuristic-fn]
+  [branch? children heuristic-fn next-step-fn]
   (letfn [(alphabeta [node depth alpha beta maximizing-player?]
             (if (or (= depth 0) (not (branch? node)))
               (heuristic-fn node)
@@ -15,7 +15,7 @@
                        cs (children node)
                        child-node nil]
                   (if (or (empty? cs) (<= beta a))
-                    (do (println child-node v) v)
+                    (do (next-step-fn child-node) v)
                     (let [child-v (alphabeta (first cs) (dec depth) a beta false)
                           v' (max v child-v)
                           a' (max a v')
@@ -26,7 +26,7 @@
                        cs (children node)
                        child-node nil]
                   (if (or (empty? cs) (<= b alpha))
-                    (do (println child-node v) v)
+                    (do (next-step-fn child-node) v)
                     (let [child-v (alphabeta (first cs) (dec depth) alpha b true)
                           v' (min v child-v)
                           b' (min b v')
@@ -86,7 +86,8 @@
 
 
 (defn children-children 
-  ""
+  "Takes a game in which it is detective d's turn and returns a possible game states
+  where the detective d has moved"
   [game]
   (let [[i detective] (g/find-detective game (g/whos-turn? game))]
     (as-> (map/neighbours-for-detective (:position detective)) x
@@ -107,13 +108,26 @@
         (g/move-mrx x "X") ;; His best choice X = Black ticket
         (vector x))
       ;; All possible detectives' moves in this round
-      (tree-seq (fn [g] (= (:round g) (:round game)))
-                children-children 
-                game))))
+      (as->  game  x
+        ;; tree of all intermediate steps
+        (tree-seq (fn [g] (= (:round g) (:round game))) children-children x) 
+        ;; Only states of the game of the next round 
+        (filter (fn [g] (not= (:round g) (:round game))) x) 
+        ))))
 
-(defn branch? [game]
+(defn branch? 
+  "Takes a game and return true if Mr. X has not to reveal"
+  [game]
   (not (g/mrx-has-to-reveal? game)))
 
-(defn next-steps-all-detectives [g]
-  (let [ab (create-alphabeta branch? children heuristic-3)]
-    (ab g 1 Long/MIN_VALUE Long/MAX_VALUE true)))
+(defn next-steps-all-detectives
+  "Take a game and a depth and returns the minimaxed next game state" 
+  ([g depth]
+   (let [next-state (atom nil)
+         next-step-fn (fn [g] (swap! next-state (fn [_] g)))
+         ab (create-alphabeta branch? children heuristic-3 next-step-fn)
+         maximize? (not= :mrx (g/whos-turn? g))]
+     (ab g depth Long/MIN_VALUE Long/MAX_VALUE maximize?)
+     (deref next-state)))
+  ([g]
+   (next-steps-all-detectives g 1)))
